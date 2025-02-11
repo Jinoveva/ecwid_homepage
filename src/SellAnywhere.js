5fcfbcc85dd66b43a817f5a55485293c13b0cbd0
Jinoveva <jinovevalopes@gmail.com>
2025-02-11 19:42:06 +0200

    Page 2
    Created my second page which is sell anywhere

---
+++ src/SellAnywhere.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Lottie from "lottie-react";
+import 'boxicons';
+import './SellAnywhere.css';
+
+function SellAnywhere() {
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default SellAnywhere;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Head from './Head';
 import SellingOnline from './SellingOnline';
-
+import SellAnywhere from './SellAnywhere';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Head />
     <SellingOnline />
+    <SellAnywhere />
   </React.StrictMode>
 );
