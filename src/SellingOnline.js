c8d748eee5f294a7f93ca3116db7b4c0a4ea0fd8
Jinoveva <jinovevalopes@gmail.com>
2025-02-11 19:13:38 +0200

    SellingOnline
    Finished off my selling online by adding lottie images and well as rewards

--- src/SellingOnline.js
+++ src/SellingOnline.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import './SellingOnline.css'
+import React from "react";
+import Lottie from "lottie-react";
+import animationData from "./mainScene.json";
+import 'boxicons';
+import "./SellingOnline.css";
 
-function SellingOnline () {
+function SellingOnline() {
     return (
-        <div>
+        <div className="selling-container">
             <h1>Start selling online for free</h1>
-            <p>We're putting free trials on trial. With Ecwid, you get free forever. Set up
-                <br/>
+            <p>
+                We're putting free trials on trial. With Ecwid, you get free forever. Set up
+                <br />
                 your free account once, and keep it as long as you like. Seriously!
             </p>
-            <button className='getStartedBtn'>Get started for free</button>
-            <p className='noCredit'>No credit card required</p>
-            {/* Come back to add animation */}
+
+            <button className="getStartedBtn">Get started for free</button>
+            <p className="noCredit">No credit card required</p>
+
+              {/* Lottie Animation */}
+             <Lottie 
+                animationData={animationData} 
+                loop={true} 
+                autoplay={true} 
+                className="lottie-animation"
+            />
+
+            <div className="awards-container">
+                <div className="award">
+                <p> <box-icon name='right-top-arrow-circle'></box-icon> Capterra</p>
+                <p>Best ease of use</p>
+                </div>
+
+                <div className="award">
+                <p> <box-icon type='solid' name='star'></box-icon> Trustpilot</p>
+                <p>4.7 average rating</p>
+                </div>
+
+                <div className="award">
+                <p> <box-icon type='logo' name='google-plus-circle'></box-icon> G CROWD</p>
+                <p>Easiest Setup Summer 2021</p>
+                </div>
+
+                <div className="award">
+                <p> <box-icon type='logo' name='google-plus-circle'></box-icon> G CROWD</p>
+                <p>Best Usability Summer 2021</p>
+                </div>
+            </div>
+
         </div>
     );
 }
